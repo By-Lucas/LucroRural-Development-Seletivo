c276890a57aa10489a1995dfa12a03e97b8f7960
by-lucas <lukasmulekezika2@gmail.com>
2022-05-31 01:17:13 -0300

    Correcao e docker

--- lucrorual_main/settings.py
+++ lucrorual_main/settings.py
@@ -89,11 +89,22 @@ WSGI_APPLICATION = 'lucrorual_main.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': config('ENGINE'),
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT')
     }
 }
 
